6f52a022e07a5fae06cf2c14cf02e13c98a94f6c
Bryce Hirschfeld <cbhirsch859@gmail.com>
2024-06-28 19:11:09 +0000

    fix: added database functionality

--- number_guess.sh
+++ number_guess.sh
@@ -13,20 +13,23 @@ GAME() {
   then
     USER_NAME=$USER_INPUT
     echo -e "\nWelcome, $USER_NAME! It looks like this is your first time here."
+    INPUT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USER_NAME')")
+    #get user_id
+    USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USER_NAME'")
 
   else
+    #get user_id
+    USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USER_NAME'")
     #get user info
-    USER_INFO=$($PSQL "SELECT games_played, best_game FROM users WHERE username='$USER_NAME'")
-    echo $USER_INFO | while IFS='|' read GAMES_PLAYED BEST_GAME
-    do 
-      echo -e "\nWelcome back, $USER_NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
-    done
+    BEST_GAME=$($PSQL "SELECT best_game FROM game_info WHERE user_id='$USER_ID'")
+    GAMES_PLAYED=$($PSQL "SELECT games_played FROM game_info WHERE user_id='$USER_ID'")
+    echo -e "\nWelcome back, $USER_NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+
   fi
   #initialize variable guessed_number
   NUMBER_FOUND=false
   #generate random number between 1-1000
   SECRET_NUMBER=$((RANDOM % 1000 +1))
-  echo $SECRET_NUMBER
   #initial user input
   echo -e "\nGuess the secret number between 1 and 1000:"
   NUMBER_OF_GUESSES=0
@@ -54,6 +57,33 @@ GAME() {
       NUMBER_FOUND=true
     fi
   done
+
+  #game finished statement
+  echo -e "\nYou guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+
+  #add info into database
+  GAME_INFO_ID=$($PSQL "SELECT game_info_id FROM game_info WHERE user_id=$USER_ID")
+  echo $GAME_INFO_ID
+  if [[ -z $GAME_INFO_ID ]]
+  then
+    INSERT_NEW_INFO=$($PSQL "INSERT INTO game_info(games_played,best_game,user_id) VALUES(1,$NUMBER_OF_GUESSES,$USER_ID)")
+  else
+    #increment games played
+    ((GAMES_PLAYED++))
+    
+    #check if best game
+    if [ $BEST_GAME -gt $NUMBER_OF_GUESSES ]
+    then
+      #if true update best game
+      BEST_GAME=$NUMBER_OF_GUESSES
+
+    fi
+
+    #add info
+    UPDATE_GAME_INFO=$($PSQL "UPDATE game_info SET games_played=$GAMES_PLAYED,best_game=$BEST_GAME WHERE game_info_id=$GAME_INFO_ID")
+
+  fi
+
 
   #game finished statement
   echo -e "\nYou guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
--- test.sh
+++ test.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Prompt the user for input
-read -p "Enter a value: " USER_INPUT
+# Simulating USER_INFO
+USER_INFO="10|5"
+USER_NAME="John"
 
-# Check if the input is not an integer
-if ! [[ "$USER_INPUT" =~ ^-?[0-9]+$ ]]; then
-  echo "The input is not an integer."
-else
-  echo "The input is an integer."
-fi
+# Read the USER_INFO
+while IFS='|' read -r GAMES_PLAYED BEST_GAME; do
+  echo -e "\nWelcome back, $USER_NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+done < <(echo "$USER_INFO")
+
+# Now you can use the BEST_GAME variable outside the loop
+echo "Best game outside the loop: $BEST_GAME"
